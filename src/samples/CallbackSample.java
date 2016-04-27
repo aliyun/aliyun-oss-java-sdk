13c175e9173838da67b577c5a3b5d6f406e43e90
yubin.byb <yubin.byb@alibaba-inc.com>
2016-04-27 18:19:14 +0800

    refine sample

---
+++ src/samples/CallbackSample.java
@@ -0,0 +1,67 @@
+package samples;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.Callback;
+import com.aliyun.oss.model.Callback.CalbackBodyType;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+
+/**
+ * 上传回调使用示例
+ *
+ */
+public class CallbackSample {
+    
+    private static String endpoint = "*** Provide OSS endpoint ***";
+    private static String accessKeyId = "*** Provide your AccessKeyId ***";
+    private static String accessKeySecret = "*** Provide your AccessKeySecret ***";
+    private static String bucketName = "*** Provide bucket name ***";
+    // 您的回调服务器地址，如http://oss-demo.aliyuncs.com:23450或http://30.2.36.126:9090
+    private static final String callbackUrl = "<yourCallbackServerUrl>";
+    
+
+    public static void main(String[] args) throws IOException {        
+
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+        
+        try {
+            String content = "Hello OSS";
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, "key", 
+                    new ByteArrayInputStream(content.getBytes())); 
+            
+            Callback callback = new Callback();
+            callback.setCallbackUrl(callbackUrl);
+            callback.setCallbackHost("oss-cn-hangzhou.aliyuncs.com");
+            callback.setCallbackBody("{\\\"mimeType\\\":${mimeType},\\\"size\\\":${size}}");
+            callback.setCalbackBodyType(CalbackBodyType.JSON);
+            callback.addCallbackVar("x:var1", "value1");
+            callback.addCallbackVar("x:var2", "value2");
+            putObjectRequest.setCallback(callback);
+            
+            PutObjectResult putObjectResult = ossClient.putObject(putObjectRequest);
+            byte[] buffer = new byte[1024];
+            putObjectResult.getCallbackResponseBody().read(buffer);
+            putObjectResult.getCallbackResponseBody().close();
+
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message: " + oe.getErrorCode());
+            System.out.println("Error Code:       " + oe.getErrorCode());
+            System.out.println("Request ID:      " + oe.getRequestId());
+            System.out.println("Host ID:           " + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ce.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+    }
+}
---
+++ src/samples/PostObjectSample.java
@@ -0,0 +1,194 @@
+package samples;
+
+import javax.activation.MimetypesFileTypeMap;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * This sample demonstrates how to post object under specfied bucket from Aliyun
+ * OSS using the OSS SDK for Java.
+ */
+public class PostObjectSample {
+
+    // 需要上传的本地文件，确保该文件存在
+    private String localFilePath = "D:\\work\\oss\\java-sdk\\java-sdk-sample\\src\\samples\\HelloOSS.java";
+    // OSS域名，如http://oss-cn-hangzhou.aliyuncs.com
+    private String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+    // AccessKey请登录https://ak-console.aliyun.com/#/查看
+    private String accessKeyId = "2NeLUvmJFYbrj2Eb";
+    private String accessKeySecret = "tpKbdpzCavhbYghxHih5urCw5lkBdx";
+    // 你之前创建的bucket，确保这个bucket已经创建
+    private String bucketName = "oss-test-byb";
+    // 上传文件后的object名称
+    private String key = "yourKey";
+
+    
+    private void PostObject() throws Exception {
+        // 提交表单的URL为bucket域名
+        String urlStr = endpoint.replace("http://", "http://" + bucketName+ ".");
+        // 表单域
+        Map<String, String> textMap = new LinkedHashMap<String, String>();
+        
+        // key
+        textMap.put("key", this.key);
+        // Content-Disposition
+        textMap.put("Content-Disposition", "attachment;filename="
+                + localFilePath);
+        // OSSAccessKeyId
+        textMap.put("OSSAccessKeyId", accessKeyId);
+        // policy
+        String policy = "{\"expiration\": \"2120-01-01T12:00:00.000Z\",\"conditions\": [[\"content-length-range\", 0, 104857600]]}";
+        String encodePolicy = new String(Base64.encodeBase64(policy.getBytes()));
+        textMap.put("policy", encodePolicy);
+        // Signature
+        String signaturecom = com.aliyun.oss.common.auth.ServiceSignature
+                .create().computeSignature(accessKeySecret, encodePolicy);
+        textMap.put("Signature", signaturecom);
+
+        Map<String, String> fileMap = new HashMap<String, String>();
+        fileMap.put("file", localFilePath);
+
+        String ret = formUpload(urlStr, textMap, fileMap);
+        
+        System.out.println("Post Object [" + this.key + "] to bucket [" + bucketName + "]");
+        System.out.println("post reponse:" + ret);
+    }
+
+    private static String formUpload(String urlStr, Map<String, String> textMap, 
+            Map<String, String> fileMap) throws Exception {
+        String res = "";
+        HttpURLConnection conn = null;
+        String boundary = "9431149156168";
+        
+        try {
+            URL url = new URL(urlStr);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(30000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("User-Agent", 
+                    "Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN; rv:1.9.2.6)");
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data; boundary=" + boundary);
+            OutputStream out = new DataOutputStream(conn.getOutputStream());
+            
+            // text
+            if (textMap != null) {
+                StringBuffer strBuf = new StringBuffer();
+                Iterator<Entry<String, String>> iter = textMap.entrySet().iterator();
+                int i = 0;
+                
+                while (iter.hasNext()) {
+                    Entry<String, String> entry = iter.next();
+                    String inputName = entry.getKey();
+                    String inputValue = entry.getValue();
+                    
+                    if (inputValue == null) {
+                        continue;
+                    }
+                    
+                    if (i == 0) {
+                        strBuf.append("--").append(boundary).append("\r\n");
+                        strBuf.append("Content-Disposition: form-data; name=\""
+                                + inputName + "\"\r\n\r\n");
+                        strBuf.append(inputValue);
+                    } else {
+                        strBuf.append("\r\n").append("--").append(boundary).append("\r\n");
+                        strBuf.append("Content-Disposition: form-data; name=\""
+                                + inputName + "\"\r\n\r\n");
+                        strBuf.append(inputValue);
+                    }
+
+                    i++;
+                }
+                out.write(strBuf.toString().getBytes());
+            }
+
+            // file
+            if (fileMap != null) {
+                Iterator<Entry<String, String>> iter = fileMap.entrySet().iterator();
+                
+                while (iter.hasNext()) {
+                    Entry<String, String> entry = iter.next();
+                    String inputName = (String) entry.getKey();
+                    String inputValue = (String) entry.getValue();
+                    
+                    if (inputValue == null) {
+                        continue;
+                    }
+                    
+                    File file = new File(inputValue);
+                    String filename = file.getName();
+                    String contentType = new MimetypesFileTypeMap().getContentType(file);
+                    if (contentType == null || contentType.equals("")) {
+                        contentType = "application/octet-stream";
+                    }
+
+                    StringBuffer strBuf = new StringBuffer();
+                    strBuf.append("\r\n").append("--").append(boundary)
+                            .append("\r\n");
+                    strBuf.append("Content-Disposition: form-data; name=\""
+                            + inputName + "\"; filename=\"" + filename
+                            + "\"\r\n");
+                    strBuf.append("Content-Type: " + contentType + "\r\n\r\n");
+
+                    out.write(strBuf.toString().getBytes());
+
+                    DataInputStream in = new DataInputStream(new FileInputStream(file));
+                    int bytes = 0;
+                    byte[] bufferOut = new byte[1024];
+                    while ((bytes = in.read(bufferOut)) != -1) {
+                        out.write(bufferOut, 0, bytes);
+                    }
+                    in.close();
+                }
+                
+                StringBuffer strBuf = new StringBuffer();
+                out.write(strBuf.toString().getBytes());
+            }
+
+            byte[] endData = ("\r\n--" + boundary + "--\r\n").getBytes();
+            out.write(endData);
+            out.flush();
+            out.close();
+
+            // 读取返回数据
+            StringBuffer strBuf = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                strBuf.append(line).append("\n");
+            }
+            res = strBuf.toString();
+            reader.close();
+            reader = null;
+        } catch (Exception e) {
+            System.err.println("Send post request exception: " + e);
+            throw e;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+                conn = null;
+            }
+        }
+        
+        return res;
+    }
+
+    public static void main(String[] args) throws Exception {
+        PostObjectSample ossPostObject = new PostObjectSample();
+        ossPostObject.PostObject();
+    }
+
+}
