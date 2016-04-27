45a37f28fd26f6a1133088c58785626e376a0ff3
yubin.byb <yubin.byb@alibaba-inc.com>
2016-04-27 18:18:05 +0800

    refine sample

--- src/samples/GetStartedSample.java
+++ src/samples/GetStartedSample.java
@@ -1,3 +1,5 @@
+package samples;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -6,16 +8,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.util.UUID;
 
 import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.Bucket;
-import com.aliyun.oss.model.GetObjectRequest;
-import com.aliyun.oss.model.ListObjectsRequest;
+import com.aliyun.oss.model.CannedAccessControlList;
+import com.aliyun.oss.model.CreateBucketRequest;
+import com.aliyun.oss.model.ListBucketsRequest;
 import com.aliyun.oss.model.OSSObject;
 import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.model.ObjectAcl;
 import com.aliyun.oss.model.ObjectListing;
 import com.aliyun.oss.model.PutObjectRequest;
 
@@ -25,43 +28,46 @@ import com.aliyun.oss.model.PutObjectRequest;
  */
 public class GetStartedSample {
     
-    private static String endpoint = "*** Provide OSS endpoint ***";
-    private static String accessKeyId = "*** Provide your AccessKeyId ***";
-    private static String accessKeySecret = "*** Provide your AccessKeySecret ***";
+    private static String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+    private static String accessKeyId = "ik3rP0m8QmsLwnVH";
+    private static String accessKeySecret = "G3ADnVCjZqPHzeK4ukpFuJHrJLMJtq";
+    private static String bucketName = "oss-test-xyd";
     
-    private static OSSClient client = null;
-
+    private static String key = "MyObjectKey";
+    
     public static void main(String[] args) throws IOException {
         /*
          * Constructs a client instance with your account for accessing OSS
          */
-        client = new OSSClient(endpoint, accessKeyId, accessKeySecret);
-
-        String bucketName = "my-first-oss-bucket" + UUID.randomUUID();
-        String key = "MyObjectKey";
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
         
-        System.out.println("===========================================");
-        System.out.println("Getting Started with OSS SDK for Java");
-        System.out.println("===========================================\n");
+        System.out.println("Getting Started with OSS SDK for Java\n");
         
         try {
+
             /*
-             * Create a new OSS bucket
+             * Determine whether the bucket exists
              */
-            System.out.println("Creating bucket " + bucketName + "\n");
-            client.createBucket(bucketName);
-            
-            /*
-             * Determine whether the newly bucket exists
-             */
-            boolean exists = client.doesBucketExist(bucketName);
-            System.out.println("Does bucket " + bucketName + " exist? " + exists + "\n");
+            if (!ossClient.doesBucketExist(bucketName)) {
+                /*
+                 * Create a new OSS bucket
+                 */
+                System.out.println("Creating bucket " + bucketName + "\n");
+                ossClient.createBucket(bucketName);
+                CreateBucketRequest createBucketRequest= new CreateBucketRequest(bucketName);
+                createBucketRequest.setCannedACL(CannedAccessControlList.PublicRead);
+                ossClient.createBucket(createBucketRequest);
+            }
 
             /*
              * List the buckets in your account
              */
             System.out.println("Listing buckets");
-            for (Bucket bucket : client.listBuckets()) {
+            
+            ListBucketsRequest listBucketsRequest = new ListBucketsRequest();
+            listBucketsRequest.setMaxKeys(500);
+            
+            for (Bucket bucket : ossClient.listBuckets()) {
                 System.out.println(" - " + bucket.getName());
             }
             System.out.println();
@@ -70,19 +76,25 @@ public class GetStartedSample {
              * Upload an object to your bucket
              */
             System.out.println("Uploading a new object to OSS from a file\n");
-            client.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
+            ossClient.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
             
             /*
              * Determine whether an object residents in your bucket
              */
-            exists = client.doesObjectExist(bucketName, key);
+            boolean exists = ossClient.doesObjectExist(bucketName, key);
             System.out.println("Does object " + bucketName + " exist? " + exists + "\n");
+            
+            ossClient.setObjectAcl(bucketName, key, CannedAccessControlList.PublicRead);
+            ossClient.setObjectAcl(bucketName, key, CannedAccessControlList.Default);
+            
+            ObjectAcl objectAcl = ossClient.getObjectAcl(bucketName, key);
+            System.out.println("ACL:" + objectAcl.getPermission().toString());
             
             /*
              * Download an object from your bucket
              */
             System.out.println("Downloading an object");
-            OSSObject object = client.getObject(new GetObjectRequest(bucketName, key));
+            OSSObject object = ossClient.getObject(bucketName, key);
             System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
             displayTextInputStream(object.getObjectContent());
 
@@ -90,8 +102,7 @@ public class GetStartedSample {
              * List objects in your bucket by prefix
              */
             System.out.println("Listing objects");
-            ObjectListing objectListing = client.listObjects(new ListObjectsRequest(bucketName)
-                    .withPrefix("My"));
+            ObjectListing objectListing = ossClient.listObjects(bucketName, "My");
             for (OSSObjectSummary objectSummary : objectListing.getObjectSummaries()) {
                 System.out.println(" - " + objectSummary.getKey() + "  " +
                                    "(size = " + objectSummary.getSize() + ")");
@@ -102,13 +113,8 @@ public class GetStartedSample {
              * Delete an object
              */
             System.out.println("Deleting an object\n");
-            client.deleteObject(bucketName, key);
-
-            /*
-             * Delete a bucket
-             */
-            System.out.println("Deleting bucket " + bucketName + "\n");
-            client.deleteBucket(bucketName);
+            ossClient.deleteObject(bucketName, key);
+            
         } catch (OSSException oe) {
             System.out.println("Caught an OSSException, which means your request made it to OSS, "
                     + "but was rejected with an error response for some reason.");
@@ -125,7 +131,7 @@ public class GetStartedSample {
             /*
              * Do not forget to shut down the client finally to release all allocated resources.
              */
-            client.shutdown();
+            ossClient.shutdown();
         }
     }
     
--- src/samples/UploadPartCopySample.java
+++ src/samples/UploadPartCopySample.java
@@ -1,3 +1,5 @@
+package samples;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
