54a9d41976462ad61a7e515d06dee9e13d18f6b9
baiyubin <yubin.byb@alibaba-inc.com>
2016-07-18 14:51:46 +0800

    version

--- src/test/java/com/aliyun/oss/common/utils/VersionUtilTest.java
+++ src/test/java/com/aliyun/oss/common/utils/VersionUtilTest.java
@@ -28,7 +28,7 @@ public class VersionUtilTest {
     @Test
     public void testGetDefaultUserAgent() { 
         String userAgent = VersionInfoUtils.getDefaultUserAgent();
-        assertTrue(userAgent.startsWith("aliyun-sdk-java/2.1.0("));
+        assertTrue(userAgent.startsWith("aliyun-sdk-java/2.3.0("));
         assertEquals(userAgent.split("/").length, 4);
         assertEquals(userAgent.split(";").length, 2);
         assertEquals(userAgent.split("\\(").length, 2);
