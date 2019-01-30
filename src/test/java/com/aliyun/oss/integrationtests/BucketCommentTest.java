6f42f3977a54c63e4492d1ef7185c5322b3b1a26
ptrdu <ptrdu@mucheng.local>
2019-01-30 15:23:07 +0800

    TEST: add test case

--- src/test/java/com/aliyun/oss/integrationtests/BucketCommentTest.java
+++ src/test/java/com/aliyun/oss/integrationtests/BucketCommentTest.java
@@ -41,4 +41,40 @@ public class BucketCommentTest extends TestBase {
       ossClient.deleteBucket(testBucketName);
     }
   }
+
+  @Test
+  public void updateBucketComment() {
+    final String bucketName = "test-comment-bucket";
+    final String oriComment = "this is a test comment";
+    final String newComment = "this is a new comment";
+
+    try {
+      CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucketName);
+      createBucketRequest.setLocationConstraint(OSS_TEST_REGION);
+      createBucketRequest.setStorageClass(StorageClass.Standard);
+      createBucketRequest.setComment(oriComment);
+      ossClient.createBucket(createBucketRequest);
+
+      BucketList bucketList = ossClient.listBuckets(bucketName, "", 100);
+      List<Bucket> buckets = bucketList.getBucketList();
+      Assert.assertEquals(1, buckets.size());
+      Assert.assertEquals(oriComment, buckets.get(0).getComment());
+
+      SetBucketCommentRequest setBucketCommentRequest = new SetBucketCommentRequest(bucketName);
+      setBucketCommentRequest.setComment(newComment);
+
+      ossClient.putBucketComment(setBucketCommentRequest);
+      BucketList newBucketList = ossClient.listBuckets(bucketName, "", 100);
+
+      List<Bucket> newBuckets = newBucketList.getBucketList();
+
+      Assert.assertEquals(1, newBuckets.size());
+      Assert.assertEquals(newComment, newBuckets.get(0).getComment());
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail(e.getMessage());
+    } finally {
+      ossClient.deleteBucket(bucketName);
+    }
+  }
 }
