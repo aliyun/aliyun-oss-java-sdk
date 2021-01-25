4e678e1301212efd48498a0a4775a0f5240e545a
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2021-01-25 10:12:53 +0800

    update EcsRamRoleProvider

--- src/test/java/com/aliyun/oss/common/provider/EcsRamRoleCredentialsProviderTest.java
+++ src/test/java/com/aliyun/oss/common/provider/EcsRamRoleCredentialsProviderTest.java
@@ -24,10 +24,12 @@ import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.common.auth.*;
 import com.aliyun.oss.common.provider.mock.EcsRamRoleCredentialsFetcherMock;
 import com.aliyun.oss.common.provider.mock.EcsRamRoleCredentialsFetcherMock.ResponseCategory;
+import com.aliyun.oss.common.utils.DateUtil;
 import junit.framework.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.Date;
 
 public class EcsRamRoleCredentialsProviderTest extends TestBase {
 
@@ -252,6 +254,71 @@ public class EcsRamRoleCredentialsProviderTest extends TestBase {
             ossClient.deleteObject(bucketName, key);
             ossClient.deleteBucket(bucketName);
             ossClient.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testExpireTime() {
+
+        String NORMAL_METADATA_1 = "{" + "\"AccessKeyId\" : \"STS.EgnR2nX****FAf9uuqjHS8Ddt\","
+                + "\"AccessKeySecret\" : \"CJ7G63EhuZuN8rfSg2Rd****qAgHMhmDuMkp****NPUV\","
+                + "\"SecurityToken\" : \"CAISigJ1q6Ft5B2yfSjIpKTbGYjatahPg6CtQ0CIkXUkZsd/14HPljz2IHBE****AOEetfs2lW1T6P0TlrRtTtpfTEmBbI569s1WqQW+Z5fT5JHo4LZfhoGoRzB9keMGTIyADd/iRfbxJ92PCTmd5AIRrJ****K9JS/HVbSClZ9gaPkOQwC8dkAoLdxKJwxk2qR4XDmrQp****PxhXfKB0dFoxd1jXgFiZ6y2cqB8BHT/jaYo603392ofsj1NJE1ZMglD4nlhbxMG/CfgHIK2X9j77xriaFIwzDDs+yGDkNZixf8aLqEqIM/dV4hPfdjSvMf8qOtj5t1sffJnoHtzBJAIexOT****FVtcH5xchqAAXp1d/dYv+2L+dJDW+2pm1vACD/UlRk93prPkyuU3zH2wnvXBxEi26QnoQSCA+T1yE2wo41V2mS+LSGYN/PC+2Ml1q+JX5DzKgfGrUPt7kU4FeXJDzGh2YaXRGpO7yERKgAc/NukkDNqthMaHntyTeix08DYBuTT6gd3V8XmN8vF\","
+                + "\"Code\" : \"Success\","
+                + "\"Expiration\" :" ;
+
+        String NORMAL_METADATA_2 = "{" + "\"AccessKeyId\" : \"STS.AgnR2nX****FAf9uuqjHS8Ddt\","
+                + "\"AccessKeySecret\" : \"AJ7G63EhuZuN8rfSg2Rd****qAgHMhmDuMkp****NPUV\","
+                + "\"SecurityToken\" : \"AAISigJ1q6Ft5B2yfSjIpKTbGYjatahPg6CtQ0CIkXUkZsd/14HPljz2IHBE****AOEetfs2lW1T6P0TlrRtTtpfTEmBbI569s1WqQW+Z5fT5JHo4LZfhoGoRzB9keMGTIyADd/iRfbxJ92PCTmd5AIRrJ****K9JS/HVbSClZ9gaPkOQwC8dkAoLdxKJwxk2qR4XDmrQp****PxhXfKB0dFoxd1jXgFiZ6y2cqB8BHT/jaYo603392ofsj1NJE1ZMglD4nlhbxMG/CfgHIK2X9j77xriaFIwzDDs+yGDkNZixf8aLqEqIM/dV4hPfdjSvMf8qOtj5t1sffJnoHtzBJAIexOT****FVtcH5xchqAAXp1d/dYv+2L+dJDW+2pm1vACD/UlRk93prPkyuU3zH2wnvXBxEi26QnoQSCA+T1yE2wo41V2mS+LSGYN/PC+2Ml1q+JX5DzKgfGrUPt7kU4FeXJDzGh2YaXRGpO7yERKgAc/NukkDNqthMaHntyTeix08DYBuTT6gd3V8XmN8vF\","
+                + "\"Code\" : \"Success\","
+                + "\"Expiration\" :" ;
+
+        try {
+
+            EcsRamRoleCredentialsFetcherMock credentialsFetcher = new EcsRamRoleCredentialsFetcherMock(
+                    TestConfig.OSS_AUTH_SERVER_HOST).withResponseCategory(ResponseCategory.FromExternal);
+
+            String meta = NORMAL_METADATA_1 + "\"" + DateUtil.formatAlternativeIso8601Date(new Date()) + "\"}";
+            credentialsFetcher.setExternalData(meta);
+
+            EcsRamRoleCredentialsProvider credentialsProvider = new EcsRamRoleCredentialsProvider(
+                    TestConfig.OSS_AUTH_SERVER_HOST).withCredentialsFetcher(credentialsFetcher);
+
+            BasicCredentials credentials = (BasicCredentials) credentialsProvider.getCredentials();
+            Assert.assertEquals(credentials.getAccessKeyId(), "STS.EgnR2nX****FAf9uuqjHS8Ddt");
+            Assert.assertEquals(credentials.getSecretAccessKey(), "CJ7G63EhuZuN8rfSg2Rd****qAgHMhmDuMkp****NPUV");
+            Assert.assertTrue(credentials.useSecurityToken());
+            Assert.assertTrue(credentials.willSoonExpire());
+
+            meta = NORMAL_METADATA_2 + "\"" + DateUtil.formatAlternativeIso8601Date(new Date()) + "\"}";
+            credentialsFetcher.setExternalData(meta);
+
+            credentials = (BasicCredentials) credentialsProvider.getCredentials();
+            Assert.assertEquals(credentials.getAccessKeyId(), "STS.AgnR2nX****FAf9uuqjHS8Ddt");
+            Assert.assertEquals(credentials.getSecretAccessKey(), "AJ7G63EhuZuN8rfSg2Rd****qAgHMhmDuMkp****NPUV");
+            Assert.assertTrue(credentials.useSecurityToken());
+            Assert.assertTrue(credentials.willSoonExpire());
+
+            long currTime = new Date().getTime() + (3600 * 6 * 15/100 - 100) * 1000;
+            meta = NORMAL_METADATA_1 + "\"" + DateUtil.formatAlternativeIso8601Date(new Date(currTime)) + "\"}";
+            credentialsFetcher.setExternalData(meta);
+            credentials = (BasicCredentials) credentialsProvider.getCredentials();
+            Assert.assertEquals(credentials.getAccessKeyId(), "STS.EgnR2nX****FAf9uuqjHS8Ddt");
+            Assert.assertEquals(credentials.getSecretAccessKey(), "CJ7G63EhuZuN8rfSg2Rd****qAgHMhmDuMkp****NPUV");
+            Assert.assertTrue(credentials.useSecurityToken());
+            Assert.assertTrue(credentials.willSoonExpire());
+
+            currTime = new Date().getTime() + (3600 * 6 * 15/100 + 100) * 1000;
+            meta = NORMAL_METADATA_1 + "\"" + DateUtil.formatAlternativeIso8601Date(new Date(currTime)) + "\"}";
+            credentialsFetcher.setExternalData(meta);
+            credentials = (BasicCredentials) credentialsProvider.getCredentials();
+            Assert.assertEquals(credentials.getAccessKeyId(), "STS.EgnR2nX****FAf9uuqjHS8Ddt");
+            Assert.assertEquals(credentials.getSecretAccessKey(), "CJ7G63EhuZuN8rfSg2Rd****qAgHMhmDuMkp****NPUV");
+            Assert.assertTrue(credentials.useSecurityToken());
+            Assert.assertFalse(credentials.willSoonExpire());
+
         } catch (Exception e) {
             e.printStackTrace();
             Assert.fail(e.getMessage());
