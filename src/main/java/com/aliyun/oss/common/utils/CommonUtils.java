813677954c4ee5b20caf2a75d0d83ce2c3151d7a
zhouao <zhouao.za@antfin.com>
2021-10-14 14:29:20 +0800

    update License Description

--- src/main/java/com/aliyun/oss/common/utils/CommonUtils.java
+++ src/main/java/com/aliyun/oss/common/utils/CommonUtils.java
@@ -1,16 +1,29 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2021 All Rights Reserved.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package com.aliyun.oss.common.utils;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- *
- * @author zhouao
- * @version $Id: CommonUtils.java, v 0.1 2021年09月30日 2:01 PM zhouao Exp $
+ * Utils for common operation.
  */
 public class CommonUtils {
     public static URI toURI(String endpoint, String defaultProtocol) throws IllegalArgumentException {
