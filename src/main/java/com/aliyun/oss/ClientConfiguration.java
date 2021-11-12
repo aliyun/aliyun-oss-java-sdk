52acbe14e68aae07f793cae61e95c980e6e27939
JokerForCode <82107935+JokerForCode@users.noreply.github.com>
2021-11-12 12:03:43 +0800

    Fix Issue #388
    Fix thread blocking cause by the default setting

--- src/main/java/com/aliyun/oss/ClientConfiguration.java
+++ src/main/java/com/aliyun/oss/ClientConfiguration.java
@@ -51,7 +51,7 @@ public class ClientConfiguration {
 
     public static final int DEFAULT_MAX_RETRIES = 3;
 
-    public static final int DEFAULT_CONNECTION_REQUEST_TIMEOUT = -1;
+    public static final int DEFAULT_CONNECTION_REQUEST_TIMEOUT = 5 * 60 * 1000;
     public static final int DEFAULT_CONNECTION_TIMEOUT = 50 * 1000;
     public static final int DEFAULT_SOCKET_TIMEOUT = 50 * 1000;
     public static final int DEFAULT_MAX_CONNECTIONS = 1024;
