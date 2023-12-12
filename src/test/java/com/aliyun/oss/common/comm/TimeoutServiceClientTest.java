69ab583d8cc3cfcce3c0d1ed6b6370ad8e9a86f5
zhuxiaolong37 <zxl01071856@alibaba-inc.com>
2023-12-12 16:15:13 +0800

    TimeoutServiceClient adds custom threads

--- src/test/java/com/aliyun/oss/common/comm/TimeoutServiceClientTest.java
+++ src/test/java/com/aliyun/oss/common/comm/TimeoutServiceClientTest.java
@@ -20,12 +20,19 @@
 package com.aliyun.oss.common.comm;
 
 import com.aliyun.oss.ClientConfiguration;
+import com.aliyun.oss.ClientException;
 import com.aliyun.oss.HttpMethod;
 import com.aliyun.oss.internal.OSSConstants;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.net.URI;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 public class TimeoutServiceClientTest {
     @Test
@@ -48,6 +55,49 @@ public class TimeoutServiceClientTest {
             TimeoutServiceClient client = new TimeoutServiceClient(config);
             client.sendRequest(request, context);
             client.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            TimeoutServiceClient client = new TimeoutServiceClient(config);
+            client.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void testTimeoutServiceClientWithThread() {
+        int processors = Runtime.getRuntime().availableProcessors();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(processors * 20, processors * 30, 60L, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(processors * 200), Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+        ClientConfiguration config = new ClientConfiguration();
+        config.setRequestTimeoutEnabled(true);
+        config.setExecutor(executor);
+
+        // cover partial codes
+        try {
+            String content = "test content";
+            byte[] contentBytes = content.getBytes(OSSConstants.DEFAULT_CHARSET_NAME);
+            ByteArrayInputStream contentStream = new ByteArrayInputStream(contentBytes);
+
+            RequestMessage request = new RequestMessage(null, null);
+            request.setEndpoint(new URI("http://localhost"));
+            request.setMethod(HttpMethod.GET);
+            request.setContent(contentStream);
+            request.setContentLength(contentBytes.length);
+            ExecutionContext context = new ExecutionContext();
+
+            TimeoutServiceClient client = new TimeoutServiceClient(config);
+            client.sendRequest(request, context);
+            client.shutdown();
+        } catch (ClientException e) {
+            e.printStackTrace();
+            assertEquals(e.getErrorCode(), "SocketException");
         } catch (Exception e) {
             e.printStackTrace();
         }
