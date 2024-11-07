5299ae5bbece86eeb49226843e7a9062be619a9e
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2024-11-07 17:34:22 +0800

    support shade

--- src/main/java/com/aliyun/oss/common/auth/RequestPresigner.java
+++
@@ -1,8 +0,0 @@
-package com.aliyun.oss.common.auth;
-
-import com.aliyun.oss.ClientException;
-import com.aliyun.oss.common.comm.RequestMessage;
-
-public interface RequestPresigner {
-    public void presign(RequestMessage request) throws ClientException;
-}
