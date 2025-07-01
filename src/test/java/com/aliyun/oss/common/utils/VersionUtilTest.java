e7f73b65c6dd1fde55fb399178f7eda889f90db2
guangjun.hgj <guangjun.hgj@alibaba-inc.com>
2025-07-01 17:31:28 +0800

    change versionn to 3.18.3.

--- src/test/java/com/aliyun/oss/common/utils/VersionUtilTest.java
+++ src/test/java/com/aliyun/oss/common/utils/VersionUtilTest.java
@@ -28,7 +28,7 @@ public class VersionUtilTest {
     @Test
     public void testGetDefaultUserAgent() {
         String userAgent = VersionInfoUtils.getDefaultUserAgent();
-        assertTrue(userAgent.startsWith("aliyun-sdk-java/3.18.2("));
+        assertTrue(userAgent.startsWith("aliyun-sdk-java/3.18.3("));
         assertEquals(userAgent.split("/").length, 4);
         assertEquals(userAgent.split(";").length, 2);
         assertEquals(userAgent.split("\\(").length, 2);
@@ -38,7 +38,7 @@ public class VersionUtilTest {
     @Test
     public void testGetVersion() {
         String version = VersionInfoUtils.getVersion();
-        assertEquals("3.18.2", version);
+        assertEquals("3.18.3", version);
     }
 }
 
